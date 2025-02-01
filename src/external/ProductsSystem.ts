0e4c2f0b83ee3605c94b2684580ce870266021f0
vitorrafael <vrsilveira07@gmail.com>
2025-02-01 17:13:36 -0300

    chore: interact with products service

--- src/external/ProductsSystem.ts
+++ src/external/ProductsSystem.ts
@@ -1,12 +1,31 @@
+import axios from "axios";
 import ProductDTO from "../core/products/dto/ProductDTO";
 import { ProductSystem } from "../interfaces/ProductSystem";
+import { StatusCode } from "./StatusCode";
 
-export default class ProductModelDataSource implements ProductSystem {
-  async findById(id: number): Promise<ProductDTO | undefined> {
-    return Promise.resolve(undefined);
+const { PRODUCTS_SERVICE_HOST, PRODUCTS_SERVICE_PORT } = process.env;
+
+export default class ProductsSystem implements ProductSystem {
+  static buildProductsServiceURL() {
+    return `http://${PRODUCTS_SERVICE_HOST}:${PRODUCTS_SERVICE_PORT}`;
   }
 
-  #createProductDTO(values: any) {
+  async findById(id: number): Promise<ProductDTO | undefined> {
+    const url = `${ProductsSystem.buildProductsServiceURL()}/products/${id}`;
+    const response = await axios.get<ProductDTO>(url);
+
+    if (response.status === StatusCode.NOT_FOUND) return undefined;
+    if (response.status === StatusCode.INTERNAL_SERVER_ERROR) {
+      console.error({
+        message: `Products Service - Internal Server Error while fetching product with id ${id}`,
+      });
+      return undefined;
+    }
+
+    return Promise.resolve(this.createProductDTO(response.data));
+  }
+
+  private createProductDTO(values: any) {
     return new ProductDTO({
       id: values.id,
       name: values.name,
